90407b5b1b08c5afca2999109bd206978f4b953d
glin <814016331@qq.com>
2024-10-19 15:23:57 +0800

    更新代码

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -20,5 +20,9 @@ export default {
 </script>
 
 <style scoped>
-
+@media (max-width: 750px){
+  img.top-banner-image.w-560px.h-560px.flex.\<xl\:-ml-120px.air\:w-488px.air\:h-488px.air\:mt-50px.pad\:order-first.pad\:ml-0.\!pad\:m-auto.phone\:hidden {
+    display: none;
+  }
+}
 </style>
